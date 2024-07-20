97a12f046f419c8194138ee50f2dbff37bb7f46f
Abraham <abukip344@gmail.com>
2024-07-20 08:40:48 -0400

    remote lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,21 @@ import requests
 import json
 
 class GetRequester:
-
+    '''initializes with url returns lis-dict'''
     def __init__(self, url):
+        '''initialize attributes'''
         self.url = url
-
+    
     def get_response_body(self):
-        pass
-
+        response = requests.get(self.url)
+        return response.content
+    
     def load_json(self):
-        pass
+        '''return python list/dict'''
+        response_dict = json.loads(self.get_response_body())
+
+        return response_dict
+    
+    
+target_data = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(target_data.load_json())
